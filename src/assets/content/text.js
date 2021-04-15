02c582125e076b5439e9a847f92b008fa8e86f91
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2021-04-15 03:53:56 -0400

    Footer mostly done, need to add social media if client desires and email address once solidified

--- src/assets/content/text.js
+++ src/assets/content/text.js
@@ -1,4 +1,4 @@
-const footerCompanySummary = "E & M Acoustic is";
+const footerCompanySummary = "E & M Acoustic LLC is a carpentry contractor company specializing in acoustical and hardware in Connecticut. We are experts in our field and have experiences working in small, private jobs all the way to massive, multi-year projects.";
 
 export {
     footerCompanySummary,
--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,13 @@ import * as footerStyles from '../styles/footer.module.scss';
 
 import { footerCompanySummary } from '../assets/content/text.js';
 
+import { Row, Col, Container } from 'react-bootstrap';
+
+import { IconContext } from '@react-icons/all-files';
+import { HiOutlinePhone } from '@react-icons/all-files/hi/HiOutlinePhone';
+import { AiOutlineHome } from '@react-icons/all-files/ai/AiOutlineHome';
+import { HiOutlineMail } from '@react-icons/all-files/hi/HiOutlineMail';
+
 const Footer = () => {
     // keeps copyright year current forever
     let today = new Date();
@@ -11,24 +18,32 @@ const Footer = () => {
 
     return (
         <footer className={footerStyles.container}>
-            <div className={footerStyles.footerInfo}>
+            <Container>
+                <Row className={footerStyles.footerInfo}>
+                    <Col sm={4}>
+                        <h4>E & M Acoustic LLC</h4>
+                        <p className={`${footerStyles.infoSpace}`}>{footerCompanySummary}</p>
+                    </Col>
+                    <Col sm={4}></Col>
+                    <Col sm={4}>
+                        <h4>Contact Info</h4>
+                        <ul className={`${footerStyles.infoSpace}`}>
+                            <IconContext.Provider value={{ size: '1.5em', title: 'Address: 79 Mill Brook Rd, East Hartford, CT, 06118' }}>
+                                <li><AiOutlineHome />79 Mill Brook Rd, East Hartford, CT 06118</li>
+                            </IconContext.Provider>
+                            <IconContext.Provider value={{ size: '1.5em', title: 'Phone Number: 860 5433160' }}>
+                                <li><HiOutlinePhone /> 860 - 543 - 3160</li> 
+                            </IconContext.Provider>
+                            <IconContext.Provider value={{ size: '1.5em', title: 'Email' }}>
+                                <li><HiOutlineMail /></li>
+                            </IconContext.Provider>
+                        </ul>
+                    </Col>
+                </Row>
                 <div>
-                    <h4>E & M Acoustic</h4>
-                    <p>{footerCompanySummary}</p>
+                    <p className={footerStyles.copyright}>Copyright © E&M Acoustic LLC {year}</p>  
                 </div>
-                <div></div>
-                <div>
-                    <h4>Contact Info</h4>
-                    <ul>
-                        <li>79 Mill Brook Rd, East Harford, CT 06118</li>
-                        <li>860 - 543 - 3160</li>
-                        <li></li>
-                    </ul>
-                </div>
-            </div>
-            <div>
-                <p className={footerStyles.copyright}>Copyright © E&M Acoustic {year}</p>  
-             </div>
+            </Container>
         </footer>
 
        
