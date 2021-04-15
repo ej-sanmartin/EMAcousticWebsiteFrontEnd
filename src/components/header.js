27bec6e1991e05c1d9a31a83ddbc4fca621fe646
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2021-04-15 01:16:45 -0400

    Added phone number and minor style changes to page links

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,8 @@ import { Link } from 'gatsby';
 
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
+import { FaPhoneAlt } from '@react-icons/all-files/fa/FaPhoneAlt';
+
 import * as headerStyles from '../styles/header.module.scss';
 
 const Header = () => {
@@ -12,8 +14,8 @@ const Header = () => {
             <Navbar collapseOnSelect expand="sm">
                 <Container>
                     <Navbar.Brand>
-                        <Link to="/">
-                            <h1 className={headerStyles.logo}>E&M Acoustic</h1>
+                        <Link className={headerStyles.logo} to="/">
+                            <h1>E&M Acoustic</h1>
                         </Link>        
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -26,19 +28,24 @@ const Header = () => {
                             </Nav.Item>
                             <Nav.Item as="li">
                                 <Nav.Link>
-                                   <Link to="/about">About</Link> 
+                                   <Link className={headerStyles.pageLinks} to="/about">About</Link> 
                                 </Nav.Link>
                             </Nav.Item>
                             <Nav.Item as="li">
                                 <Nav.Link>
-                                    <Link to="/portfolio">Portfolio</Link>
+                                    <Link className={headerStyles.pageLinks} to="/portfolio">Portfolio</Link>
                                 </Nav.Link>
                             </Nav.Item>
                             <Nav.Item as="li">
                                 <Nav.Link>
-                                    <Link to="/contact">Contact</Link>
+                                    <Link className={headerStyles.pageLinks} to="/contact">Contact</Link>
                                 </Nav.Link>
-                            </Nav.Item> 
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link>
+                                <a className={headerStyles.pageLinks} href="tel:860-543-3160"><p>CALL NOW <FaPhoneAlt className={headerStyles.phoneIcon} />860-543-3160</p></a>
+                                </Nav.Link>
+                            </Nav.Item>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
