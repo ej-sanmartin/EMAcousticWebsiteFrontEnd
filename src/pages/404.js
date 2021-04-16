e0c98aaf341bb4728e0913e1f71d017838744b4e
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2021-04-16 03:42:23 -0400

    Added error page

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const NotFound = () => {
+    return (
+        <Layout>
+            <p>Error</p>
+        </Layout>
+    );
+}
+
+export default NotFound;
