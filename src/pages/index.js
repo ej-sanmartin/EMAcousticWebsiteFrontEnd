4fae9161ade9bf8eec91bbe52e41003ec0f10ad2
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2021-04-19 04:18:32 -0400

    Started home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,15 @@ import React from "react";
 
 import Layout from '../components/layout';
 
+import { StaticImage } from 'gatsby-plugin-image';
+
 const Home = () => {
   return (
     <Layout>
-      <h1>Hey</h1>
+      <StaticImage
+        src='../assets/images/work_makes_better_futures.jpg'
+        alt='Dramatic Construction Worker'
+      />
     </Layout>
   );
 }
