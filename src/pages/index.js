42f2ba1b28ac070e28fd276ad90388b17e03377b
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2021-04-19 05:42:56 -0400

    Added opening image to home page, next add animationto opening blurb

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,13 +4,20 @@ import Layout from '../components/layout';
 
 import { StaticImage } from 'gatsby-plugin-image';
 
+import * as homeStyles from '../styles/home.module.scss';
+
 const Home = () => {
   return (
     <Layout>
       <StaticImage
         src='../assets/images/work_makes_better_futures.jpg'
         alt='Dramatic Construction Worker'
+        className={homeStyles.openingImage}
       />
+      <div className={homeStyles.openingMessageContainer}>
+        <h2 className={homeStyles.firstOpeningPhrase}>Over 20 years of excellence</h2>
+        <h2 className={homeStyles.secondOpeningPhrase}>We take pride in our impact</h2>
+      </div>
     </Layout>
   );
 }
