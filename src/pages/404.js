56a07f572c6e23d7b143a0e1b7ca1374eda73b89
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2021-05-05 03:59:53 -0400

    Completed finished 404 error page, media queries included

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from '../components/layout';
+import NonHomeLayout from '../components/nonHomeLayout';
+
+import * as errorPageStyles from '../styles/errorPage.module.scss';
 
 const NotFound = () => {
     return (
-        <Layout>
-            <p>Error</p>
-        </Layout>
+        <NonHomeLayout>
+            <div className={`${errorPageStyles.openingImage} ${errorPageStyles.parallax}`}></div>
+            <div className={errorPageStyles.openingMessageContainer}>
+                <h2>DANGER</h2>
+                <h3>YOU HAVE GOTTEN LOST AND ARE ENTERING A <span>RESTRICTED AREA</span></h3>
+                <p>Allow us to take you someplace safe</p>
+                <Link className={errorPageStyles.returnButton} to="/">RETURN</Link>
+            </div>
+        </NonHomeLayout>
     );
 }
 
