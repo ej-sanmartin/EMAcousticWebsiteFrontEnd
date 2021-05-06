48f858574773db86451ac766aa1c9e5b62625a8a
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2021-05-05 20:56:30 -0400

    Fixed helper function encode

--- src/utilities/helpers.js
+++ src/utilities/helpers.js
@@ -1,7 +1,8 @@
 // Helper function to format submissions the way Netlify expects it
 const encode = (data) => {
     return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=")
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
 export {
