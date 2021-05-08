f539ac25c5c00d763ea5ddbf56afd88b3256702f
Edgar Jr San Martin <edgar.sanmartinjr@gmail.com>
2021-05-08 17:42:28 -0400

    Trying to fix navbar padding

--- src/components/header.js
+++ src/components/header.js
@@ -53,7 +53,7 @@ const Header = () => {
                                 </Nav.Link>
                             </Nav.Item>
                             <Nav.Item>
-                                <Nav.Link as="a" className={headerStyles.pageLinks}>
+                                <Nav.Link>
                                     <a className={headerStyles.pageLinks} href="tel:860-8888-1111">CALL NOW <FaPhoneAlt className={headerStyles.phoneIcon} style={{ fill: "white" }} />860-888-1111</a>
                                 </Nav.Link>
                             </Nav.Item>
